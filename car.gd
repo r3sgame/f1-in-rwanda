d728f77a75c82402720fc8ee283272551b7125d0
r3sgame <68745659+r3sgame@users.noreply.github.com>
2025-04-01 22:48:17 -0400

    init

--- car.gd
+++ car.gd
@@ -2,6 +2,8 @@ extends RigidBody3D
 
 var active = false
 var time = -8
+var forward_force = 15000
+var backward_force = 10000
 
 func _ready() -> void:
 	await get_tree().create_timer(8.0).timeout
@@ -11,9 +13,9 @@ func _physics_process(delta: float) -> void:
 	if active:
 		time += delta
 		if (Input.is_key_pressed(KEY_UP)):
-			apply_force(Vector3(-17500*cos(rotation.y), 0, 17500*sin(rotation.y)))
+			apply_force(Vector3(-forward_force*cos(rotation.y), 0, forward_force*sin(rotation.y)))
 		if (Input.is_key_pressed(KEY_DOWN)):
-			apply_force(Vector3(10600*cos(rotation.y), 0, -10600*sin(rotation.y)))
+			apply_force(Vector3(backward_force*cos(rotation.y), 0, -backward_force*sin(rotation.y)))
 		if (Input.is_key_pressed(KEY_LEFT)):
 			angular_velocity = Vector3(0, 1, 0)
 		elif (Input.is_key_pressed(KEY_RIGHT)):
