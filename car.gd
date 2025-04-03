0d56b72145cc4f14291981d291ee12b49e5de02e
r3sgame <68745659+r3sgame@users.noreply.github.com>
2025-04-03 11:05:04 -0400

    Game Loop

--- car.gd
+++ car.gd
@@ -1,17 +1,24 @@
 extends RigidBody3D
 
 var active = false
-var time = -8
-var forward_force = 15000
+var time = 0
+var forward_force = 16500
 var backward_force = 10000
 
 func _ready() -> void:
-	await get_tree().create_timer(8.0).timeout
+	await get_tree().create_timer(7.0).timeout
 	active = true
 
 func _physics_process(delta: float) -> void:	
+	get_parent().get_node("HUD/Speed Indicator/Speed").text = "[center]" + str(round(2.237 * sqrt(pow(linear_velocity.x, 2) + pow(linear_velocity.z, 2)))) + " MPH"
+		
+	if (Input.is_key_pressed(KEY_R)):
+		get_tree().reload_current_scene()
+	
 	if active:
 		time += delta
+		get_parent().get_node("HUD/Clock Holder/Clock").text = "%10.2f" % time
+		
 		if (Input.is_key_pressed(KEY_UP)):
 			apply_force(Vector3(-forward_force*cos(rotation.y), 0, forward_force*sin(rotation.y)))
 		if (Input.is_key_pressed(KEY_DOWN)):
@@ -24,5 +31,7 @@ func _physics_process(delta: float) -> void:
 			angular_velocity = Vector3(0, 0, 0)
 
 func _on_finish_body_entered(body: Node3D) -> void:
-	print("finish")
-	active = false# Replace with function body.
+	if body.name == "Car":
+		print("finish")
+		active = false# Replace with function body.
+		get_parent().get_node("HUD/AnimationPlayer").play("Finish")
